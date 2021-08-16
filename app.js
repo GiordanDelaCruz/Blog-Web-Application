032f886557d681391a7780d5fae8c6e049e8b881
GiordanDelaCruz <GiordanDelaCruz@gmail.com>
2021-08-16 16:59:07 -0400

    Completed Challenge #18.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
+// Load Dependencies
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 // Declare global variables
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -9,11 +12,13 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 let postArr = [];
 
-const app = express();
-
+// EJS
 app.set('view engine', 'ejs');
 
+// BodyParser
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Static file usage
 app.use(express.static("public"));
 
 // Load Homepage
@@ -51,13 +56,18 @@ app.get("/compose", function(req, res){
 // Load Post pages
 app.get("/posts/:postName", function(req, res){
 
-  // Check if any posts share the same name as the postName parameter
-  for( let i in postArr){
-    console.log("post:", postArr[i], "postName:", req.params.postName);
-    if( req.params.postName === postArr[i].postTitle ){
-      console.log("Matching")
-    }
-  }
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  postArr.forEach(function(post){
+    const storedTitle = _.lowerCase(post.postTitle);
+
+     if( requestedTitle === storedTitle ){
+       console.log("Matching");
+     }else{
+       console.log("No Match");
+     }
+
+  });
 
 });
 
